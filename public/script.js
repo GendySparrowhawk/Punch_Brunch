2285d79e02d748c577fddbd7b5fee945892a21c0
jacob hoefer <jacob.hoefer@gmail.com>
2024-02-13 14:15:43 -0500

    splash page done need dropdowns working

--- public/script.js
+++ public/script.js
@@ -1,16 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   const punchText = document.getElementById("punchText");
   const brunchText = document.getElementById("brunchText");
-  const leftImage = document.querySelector(".left-image");
-  const rightImage = document.querySelector(".right-image");
 
   punchText.addEventListener("click", toggleImages);
-
   brunchText.addEventListener("click", toggleImages);
 
   function toggleImages() {
-    leftImage.classList.toggle("fade-out-left");
-    rightImage.classList.toggle("fade-out-right");
+    console.log("clicked");
+    const leftImage = document.querySelector(".left-image").classList;
+    const rightImage = document.querySelector(".right-image").classList;
+    setTimeout(() => {
+      leftImage.replace("left-image", "leave-left");
+      rightImage.replace("right-image", "leave-right");
+      console.log("animation complete");
+    }, 100);
+
     setTimeout(loadHomeContent, 1000);
   }
 
@@ -18,8 +22,35 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch("home.html")
       .then((res) => res.text())
       .then((data) => {
+        const parser = new DOMParser();
+        const newDocument = parser.parseFromString(data, "text/html");
+        document.open();
+        document.write(newDocument.documentElement.outerHTML);
+        document.close();
         document.body.innerHTML = data;
+
+        attachDropdown();
       })
       .catch((err) => console.error("could not fetch home", err));
+  }
+  attachDropdown();
+
+  function attachDropdown() {
+   
+    document.addEventListener("click", function (e) {
+      if (e.target.matches(".dropdown-btn")) {
+        const dropdownContent = e.target.nextElementSibling;
+        console.log("btn drop");
+        dropdownContent.classList.toggle("show");
+      } else if (!e.target.matches(".dropdown-content")) {
+        
+        const dropdownContents = document.querySelectorAll(".dropdown-content");
+        dropdownContents.forEach(function (content) {
+          if (content.classList.contains("show")) {
+            content.classList.remove("show");
+          }
+        });
+      }
+    });
   }
 });
