be47b32f7be82c5ad56eb1208c4fcdd09f742e76
jacob hoefer <jacob.hoefer@gmail.com>
2024-02-15 14:07:37 -0500

    deploy ready tweaks to come

--- public/script.js
+++ public/script.js
@@ -1,56 +1,76 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const punchText = document.getElementById("punchText");
-  const brunchText = document.getElementById("brunchText");
+const punchText = document.getElementById("punchText");
+const brunchText = document.getElementById("brunchText");
 
-  punchText.addEventListener("click", toggleImages);
-  brunchText.addEventListener("click", toggleImages);
+punchText.addEventListener("click", toggleImages);
+brunchText.addEventListener("click", toggleImages);
 
-  function toggleImages() {
-    console.log("clicked");
-    const leftImage = document.querySelector(".left-image").classList;
-    const rightImage = document.querySelector(".right-image").classList;
-    setTimeout(() => {
-      leftImage.replace("left-image", "leave-left");
-      rightImage.replace("right-image", "leave-right");
-      console.log("animation complete");
-    }, 100);
+function toggleImages() {
+  console.log("clicked");
+  const leftImage = document.querySelector(".left-image").classList;
+  const rightImage = document.querySelector(".right-image").classList;
+  setTimeout(() => {
+    leftImage.replace("left-image", "leave-left");
+    rightImage.replace("right-image", "leave-right");
+    console.log("animation complete");
+  }, 100);
 
-    setTimeout(loadHomeContent, 1000);
-  }
+  setTimeout(loadHomeContent, 1000);
+}
 
-  function loadHomeContent() {
-    fetch("home.html")
-      .then((res) => res.text())
-      .then((data) => {
-        const parser = new DOMParser();
-        const newDocument = parser.parseFromString(data, "text/html");
-        document.open();
-        document.write(newDocument.documentElement.outerHTML);
-        document.close();
-        document.body.innerHTML = data;
+function loadHomeContent() {
+  fetch("home.html")
+    .then((res) => res.text())
+    .then((data) => {
+      document.open();
+      document.write(data);
+      document.close();
+      // const parser = new DOMParser();
+      // const newDocument = parser.parseFromString(data, "text/html");
+      // document.open();
+      // document.write(newDocument.documentElement.outerHTML);
+      // document.close();
+      // document.body.innerHTML = data;
+      console.log("Event listener being attached");
+      document
+        .querySelector("#submitForm")
+        .addEventListener("click", function (e) {
+          e.preventDefault();
+          document.querySelector("#submitImg").src = "./assets/eatwaffle.png";
+          console.log("submited");
 
-        attachDropdown();
-      })
-      .catch((err) => console.error("could not fetch home", err));
-  }
-  attachDropdown();
-
-  function attachDropdown() {
-   
-    document.addEventListener("click", function (e) {
-      if (e.target.matches(".dropdown-btn")) {
-        const dropdownContent = e.target.nextElementSibling;
-        console.log("btn drop");
-        dropdownContent.classList.toggle("show");
-      } else if (!e.target.matches(".dropdown-content")) {
-        
-        const dropdownContents = document.querySelectorAll(".dropdown-content");
-        dropdownContents.forEach(function (content) {
-          if (content.classList.contains("show")) {
-            content.classList.remove("show");
-          }
+          document
+            .querySelector("#submitForm")
+            .addEventListener("mouseup", function (e) {
+              e.preventDefault();
+              document.getElementById("submitButtonImage").src =
+                "./assets/waffleWhole.png";
+            });
+          const formElement = document.querySelector("#mailer");
+          const formData = new FormData(formElement);
+          console.log(formData);
+          fetch("/send-email", {
+            method: "POST",
+            body: formData,
+          })
+            .then((res) => {
+              if (res.ok) {
+                const modal = document.querySelector("#myModal");
+                modal.style.display = "block";
+                document
+                  .querySelector("#closeBtn")
+                  .addEventListener("click", function () {
+                    modal.style.display = "none";
+                  });
+                document.querySelector("#mailer").reset();
+              } else {
+                alert("error sending tell jacob to fix it");
+              }
+            })
+            .catch((err) => {
+              console.err("Error", err);
+              alert("oops jacob didnt fix this code");
+            });
         });
-      }
-    });
-  }
-});
+    })
+    .catch((err) => console.error("could not fetch home", err));
+}
--- server.js
+++ server.js
@@ -1,11 +1,63 @@
-const express = require('express')
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const upload = multer();
 const PORT = process.env.PORT || 3333;
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
 
-app.use(express.static('./public'))
+// enable CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+app.use(express.static("./public"));
 
+app.post("/send-email", upload.none(), (req, res) => {
+  //   console.log(req);
+  const { name, email, plusOne, notes } = req.body;
+  //   console.log(req.body);
 
-app.listen(PORT, () => console.log(`server started on ${PORT}`))
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "jacob.hoefer@gmail.com",
+      pass: "vobr qfxu gbpm ltlp",
+    },
+  });
+
+  let mailOptions = {
+    from: "jacob.hoefer@gmail.com",
+    to: "jacob.hoefer@gmail.com",
+    subject: "New RSVP",
+    text: `
+    Name: ${name}
+    Emial: ${email}
+    Plus One: ${plusOne}
+    Notes: ${notes}
+    `,
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      console.error("Email did not send", err);
+      res.status(500).send("error in sedning email");
+    } else {
+      console.log("Emial sent: ", info.response);
+      res.status(200).send("Email sent successfully");
+    }
+  });
+});
+
+app.listen(PORT, () => console.log(`server started on ${PORT}`));
