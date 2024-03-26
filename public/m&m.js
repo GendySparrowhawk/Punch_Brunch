c2cbd9f44ba44b242889d554c43d4527ceec78cc
jacob hoefer <jacob.hoefer@gmail.com>
2024-03-26 10:40:07 -0400

    added request a movie feature, fixed a few mobile things for clarity

--- public/m&m.js
+++ public/m&m.js
@@ -1,16 +1,51 @@
 const dropdownToggle = document.querySelector("#dropdownToggle");
 const dropdownContent = document.querySelector("#dropdownContent");
+const requestForm = document.querySelector("#Films");
+const submitBtn = document.querySelector("#submitForm")
+const modal = document.querySelector("#myModal");
+const closeBtn = document.querySelector("#closeBtn");
+
+submitBtn.addEventListener("click", submit);
 dropdownToggle.addEventListener("click", dropdown);
 
-
-
 function dropdown() {
-    if (dropdownContent.classList.contains("show")) {
-      console.log("shown");
-      dropdownContent.classList.remove("show");
-      dropdownContent.classList.add("hidden");
-    } else {
-      dropdownContent.classList.remove("hidden");
-      dropdownContent.classList.add("show");
-    }
+  if (dropdownContent.classList.contains("show")) {
+    console.log("shown");
+    dropdownContent.classList.remove("show");
+    dropdownContent.classList.add("hidden");
+  } else {
+    dropdownContent.classList.remove("hidden");
+    dropdownContent.classList.add("show");
   }
+}
+
+function submit(e) {
+  e.preventDefault();
+  console.log("submited");
+
+  const formData = new FormData(requestForm);
+  console.log(formData);
+  fetch("https://getform.io/f/6388ce58-e971-4876-a25c-693527db2114", {
+    method: "POST",
+    body: formData,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((res) => {
+      if (res.ok) {
+        document.querySelector("#submitImg").src = "./assets/eatwaffle.png";
+        modal.style.display = "block";
+        closeBtn.addEventListener("click", function () {
+          modal.style.display = "none";
+          requestForm.reset();
+        });
+      } else {
+        alert("error sending request, tell jacob to fix it");
+      }
+    })
+    .catch((err) => {
+      console.error("Error", err);
+      alert("oops jacob didnt fix this code mjs.53");
+    });
+}
