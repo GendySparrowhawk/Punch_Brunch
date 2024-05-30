853d38b784536e764b2aae3f69d1a67c21319632
jacob hoefer <jacob.hoefer@gmail.com>
2024-05-30 10:23:59 -0400

    simpified js, new date

--- public/script.js
+++ public/script.js
@@ -1,9 +1,12 @@
 const punchText = document.getElementById("punchText");
 const brunchText = document.getElementById("brunchText");
-const targetDate = new Date(2024, 4, 25, 11, 0, 0).getTime();
+const targetDate = new Date(2024, 5, 8, 11, 0, 0).getTime();
 const countDown = document.querySelector("#countdown");
 const dropdownToggle = document.querySelector("#dropdownToggle");
+const submitBtn = document.querySelector("#submitForm");
+const formElement = document.querySelector("#mailer");
 
+// set countdown for splash page
 setInterval(() => {
   const now = new Date().getTime();
   const distance = targetDate - now;
@@ -17,22 +20,12 @@ setInterval(() => {
   countDown.innerHTML = `Time till next brunch: ${days}d ${hours}h ${minutes}m ${seconds}s`;
 }, 1000);
 
-punchText.addEventListener("click", toggleImages);
-brunchText.addEventListener("click", toggleImages);
+// attaching event listners
 dropdownToggle.addEventListener("click", dropdown);
-
-function toggleImages() {
-  console.log("clicked");
-  const leftImage = document.querySelector(".left-image").classList;
-  const rightImage = document.querySelector(".right-image").classList;
-  setTimeout(() => {
-    leftImage.replace("left-image", "leave-left");
-    rightImage.replace("right-image", "leave-right");
-    console.log("animation complete");
-  }, 100);
-
-  setTimeout(loadHomeContent, 1000);
-}
+submitBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  submit();
+});
 
 function dropdown() {
   const dropdownContent = document.querySelector("#dropdownContent");
@@ -46,61 +39,41 @@ function dropdown() {
   }
 }
 
-function loadHomeContent() {
-  fetch("home.html")
-    .then((res) => res.text())
-    .then((data) => {
-      document.open();
-      document.write(data);
-      document.close();
-      console.log("Event listener being attached");
-      const dropdownContent = document.querySelector("#dropdownContent");
-      const dropdownToggle = document.querySelector("#dropdownToggle");
-      dropdownToggle.addEventListener("click", dropdown);
-
-      document
-        .querySelector("#submitForm")
-        .addEventListener("click", function (e) {
-          e.preventDefault();
-          document.querySelector("#submitImg").src = "./assets/eatwaffle.png";
-          console.log("submited");
-
-          document
-            .querySelector("#submitForm")
-            .addEventListener("mouseup", function (e) {
-              e.preventDefault();
-              document.getElementById("submitButtonImage").src =
-                "./assets/waffleWhole.png";
-            });
-          const formElement = document.querySelector("#mailer");
-          const formData = new FormData(formElement);
-          console.log(formData);
-          fetch("https://getform.io/f/6388ce58-e971-4876-a25c-693527db2114", {
-            method: "POST",
-            body: formData,
-            headers: {
-              Accept: "application/json",
-            },
-          })
-            .then((res) => {
-              if (res.ok) {
-                const modal = document.querySelector("#myModal");
-                modal.style.display = "block";
-                document
-                  .querySelector("#closeBtn")
-                  .addEventListener("click", function () {
-                    modal.style.display = "none";
-                  });
-                document.querySelector("#mailer").reset();
-              } else {
-                alert("error sending tell jacob to fix it");
-              }
-            })
-            .catch((err) => {
-              console.err("Error", err);
-              alert("oops jacob didn't fix this code");
-            });
-        });
+function submit() {
+  document
+    .querySelector("#submitForm")
+    .addEventListener("mouseup", function (e) {
+      e.preventDefault();
+      document.getElementById("submitButtonImage").src =
+        "./assets/waffleWhole.png";
+    });
+  const formData = new FormData(formElement);
+  console.log(formData);
+  fetch("https://getform.io/f/6388ce58-e971-4876-a25c-693527db2114", {
+    method: "POST",
+    body: formData,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((res) => {
+      if (res.ok) {
+        const modal = document.querySelector("#myModal");
+        modal.style.display = "block";
+        document
+          .querySelector("#closeBtn")
+          .addEventListener("click", function () {
+            modal.style.display = "none";
+          });
+        document.querySelector("#submitImg").src = "./assets/eatwaffle.png";
+        console.log("submited");
+        document.querySelector("#mailer").reset();
+      } else {
+        alert("error sending tell jacob to fix it");
+      }
     })
-    .catch((err) => console.error("could not fetch home", err));
+    .catch((err) => {
+      console.err("Error", err);
+      alert("oops jacob didn't fix this code");
+    });
 }
